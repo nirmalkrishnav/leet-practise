1b605e6c0bfdf9358ff5525aae25b901c143d414
Nirmal Krishna <nirmalkrishnav@gmail.com>
2023-09-13 00:05:46 +0530

    practise

--- todo_binary-search.js
+++ todo_binary-search.js
@@ -1,22 +1,23 @@
-// binary search
-// to do
-const binarySearch = (target, nums, start, end) => {
-  let middleIndex = Math.floor(nums.length / 2);
+const main = () => {
+  let nums = [1, 2, 3, 14, 15, 16, 27];
+  let target = 1;
+  let result = binarySearch(0, nums.length - 1, target, nums);
+  console.log('\n',result);
+};
+
+const binarySearch = (start, end, target, nums) => {
+  let middleIndex = Math.floor((start+end)/2);
+  if (start > end) {
+    return -1;
+  }
 
   if (nums[middleIndex] === target) {
     return middleIndex;
-  } else if (nums[middleIndex] >= nums[start]) {
-    return binarySearch(target, nums, start, middleIndex);
+  } else if (target < nums[middleIndex]) {
+    return binarySearch(start, middleIndex - 1, target, nums);
   } else {
-    return binarySearch(target, nums, middleIndex, end);
+    return binarySearch(middleIndex + 1, end, target, nums);
   }
 };
 
-// console.log(binarySearch(1, [1, 2, 3, 4, 5, 6, 7, 8, 9]));
-// console.log(binarySearch(2, [1, 2, 3, 4, 5, 6, 7, 8, 9]));
-console.log(binarySearch(4, [1, 2, 3, 4, 5, 6, 7, 8, 9], 0, 8));
-// console.log(binarySearch(5, [1, 2, 3, 4, 5, 6, 7, 8, 9]));
-// console.log(binarySearch(6, [1, 2, 3, 4, 5, 6, 7, 8, 9]));
-// console.log(binarySearch(7, [1, 2, 3, 4, 5, 6, 7, 8, 9]));
-// console.log(binarySearch(8, [1, 2, 3, 4, 5, 6, 7, 8, 9]));
-// console.log(binarySearch(9, [1, 2, 3, 4, 5, 6, 7, 8, 9]));
+main();
